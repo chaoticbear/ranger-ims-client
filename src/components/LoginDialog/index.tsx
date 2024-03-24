645be5e02d87d1b9ce0555c57a69e56f428d2d32
wood ape <go@revoltmedia.com>
2024-03-24 15:07:25 -0700

    Fix LoginDialog enter key

--- src/components/LoginDialog/index.tsx
+++ src/components/LoginDialog/index.tsx
@@ -1,7 +1,7 @@
 import { useAuth } from '../../hooks'
 import type { Login as LoginProps } from '../../types'
 import { IonButton, IonContent, IonInput, IonItem, IonList } from '@ionic/react'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 type LoginDialogProps = {
   setShowModal: (value: boolean) => void
@@ -10,8 +10,30 @@ type LoginDialogProps = {
 const LoginDialog: React.FC<LoginDialogProps> = ({ setShowModal }) => {
   const [ username, setUsername ] = useState('')
   const [ password, setPassword ] = useState('')
+  const [ disabled, setDisabled ] = useState(true)
+
+  useEffect(() => {
+    const keyHandler = (event: KeyboardEvent) => {
+      if (event.key === 'Enter') {
+        event.preventDefault()
+
+        handleSubmit({ password, username })
+      }
+    }
+    document.addEventListener('keyup', keyHandler)
+
+    return () => {
+      document.removeEventListener('keyup', keyHandler)
+    }
+  }, [])
+
+  useEffect(() => {
+    if(password !== '' && username !== '') setDisabled(false)
+  }, [password, username])
 
   const handleSubmit = async ({ password, username }: LoginProps) => {
+    if (disabled) return
+
     const {success} = await useAuth({password, username})
 
     success && setShowModal(false)
@@ -21,13 +43,13 @@ const LoginDialog: React.FC<LoginDialogProps> = ({ setShowModal }) => {
     <IonContent>
       <IonList>
         <IonItem>
-          <IonInput label="Handle" labelPlacement="floating" onIonChange={(e) => setUsername(e.detail.value || username)} value={username}></IonInput>
+          <IonInput label="Handle" labelPlacement="floating" onIonInput={(e) => setUsername(e.detail.value || username)} value={username}></IonInput>
         </IonItem>
         <IonItem>
-          <IonInput label="Password" labelPlacement="floating"  onIonChange={(e) => setPassword(e.detail.value || password)} type="password" value={password}></IonInput>
+          <IonInput label="Password" labelPlacement="floating"  onIonInput={(e) => setPassword(e.detail.value || password)} type="text" value={password}></IonInput>
         </IonItem>
       </IonList>
-      <IonButton onClick={() => handleSubmit({ password, username })}>Login</IonButton>
+      <IonButton disabled={disabled} onClick={() => handleSubmit({ password, username })}>Login</IonButton>
     </IonContent>
   )
 }
